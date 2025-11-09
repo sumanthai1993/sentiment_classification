6c7343213161ac468fbb07bc754359cdafe0ea47
Sumanth pichika <sumanthai1993@gmail.com>
2025-11-09 15:51:09 +0530

    replaced neutral sentiment with happiness

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -9,9 +9,9 @@ df = pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-mas
 
 df.drop(columns=['tweet_id'],inplace=True)
 
-final_df = df[df['sentiment'].isin(['neutral','sadness'])]
+final_df = df[df['sentiment'].isin(['happiness','sadness'])]
 
-final_df['sentiment'].replace({'neutral':1, 'sadness':0},inplace=True)
+final_df['sentiment'].replace({'happiness':1, 'sadness':0},inplace=True)
 
 train_data, test_data = train_test_split(final_df, test_size=0.2, random_state=42)
 
